f89c35530c01b6279855b6cd9297b2ea337e3a21
lowresr0hannt <bdwsjrgwcp@privaterelay.appleid.com>
2026-06-06 19:04:50 +0530

    banks

--- type.py
+++ type.py
@@ -1,23 +1,29 @@
-from datetime import datetime
-class User:
-    def __init__(self,username,email,password):
-        self.username=username
-        self._email=email
-        self.password=password
-        
-    def get_email(self):
-        print(f"Email accessed at {datetime.now()}")
-        return self._email
+class BankAccount:
+    MIN_BALANCE = 100
     
-    def set_email(self,new_email):
-        self._email=new_email
-    
+    def __init__(self, owner, balance=0):
+        self.owner = owner
+        self._balance =balance
         
-        
-user1 =User("Rohan","rohan@mail.com","rohanyes1")
+    def deposit (self,amount):
+        if amount > 0:
+            self._balance += amount
+            print(f"{self.owner}'s new balance: {self._balance}")
+        else:
+            print("deposit amount must be positive")
+            
+    @staticmethod
+    def is_valid_int_rate(rate):
+        return 0 <= rate <= 5
 
+
+account = BankAccount("kat",500)
+account.deposit(2000000000)
+
+print(BankAccount.is_valid_int_rate(3))
+print(BankAccount.is_valid_int_rate(10))
+
+<<<<<<< HEAD
 print(user1.get_email())
-
-user1.set_email("rohan@hotlook.com")
-
-print(user1.get_email())
+=======
+>>>>>>> c15947e (bank)
